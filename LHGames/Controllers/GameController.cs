e55c98b20bd4c59f937b44be5547d44c40703f18
DominicBg <DominicBg@users.noreply.github.com>
2017-10-07 14:34:40 -0400

    Gather ressources

--- LHGames/Controllers/GameController.cs
+++ LHGames/Controllers/GameController.cs
@@ -9,17 +9,130 @@
     public class GameController : Controller
     {
         AIHelper player = new AIHelper();
+        GameInfo gameInfo;
+        bool goingHome = false;
 
         [HttpPost]
         public string Index([FromForm]string map)
         {
-            GameInfo gameInfo = JsonConvert.DeserializeObject<GameInfo>(map);
+            gameInfo = JsonConvert.DeserializeObject<GameInfo>(map);
             var carte = AIHelper.DeserializeMap(gameInfo.CustomSerializedMap);
-            
             // INSERT AI CODE HERE.
 
-            string action = AIHelper.CreateMoveAction(gameInfo.Player.Position);
+            if (gameInfo.Player.CarriedResources >= gameInfo.Player.CarryingCapacity)
+                goingHome = true;
+
+            Tile closestResource = FindClosest(TileContent.Resource, carte);
+            Point nextPoint = (goingHome) ? GoToDestination(gameInfo.Player.HouseLocation) : GoToDestination(closestResource);
+            string action = AIHelper.CreateMoveAction(nextPoint);
+
+          
+
+            if (IsNearTile(closestResource) && !goingHome)
+            {
+                if (gameInfo.Player.CarriedResources < gameInfo.Player.CarryingCapacity)
+                {
+                    action = AIHelper.CreateCollectAction(nextPoint);
+                }
+            }
+
+            if (goingHome)
+            {
+                if (IsSameTile(gameInfo.Player.HouseLocation))
+                {
+                    goingHome = false;
+                }
+            }
+                    // action = AIHelper.CreateMoveAction(nextPoint = new Point(29,gameInfo.Player.Position.Y-1));
+            Console.WriteLine(action);
+            Console.WriteLine(goingHome + "is going home");
             return action;
         }
+
+        bool IsSameTile(Point tile)
+        {
+            if (gameInfo.Player.Position.X == tile.X && gameInfo.Player.Position.Y == tile.Y)
+                return true;
+            else
+                return false;
+        }
+
+        bool IsSameTile(Tile tile)
+        {
+            if (gameInfo.Player.Position.X == tile.X && gameInfo.Player.Position.Y == tile.Y)
+                return true;
+            else
+                return false;
+        }
+
+        bool IsNearTile(Tile tile)
+        {
+            if (Math.Abs(gameInfo.Player.Position.X - tile.X) == 1 && Math.Abs(gameInfo.Player.Position.Y - tile.Y) == 0)
+                return true;
+            else if (Math.Abs(gameInfo.Player.Position.X - tile.X) == 0 && Math.Abs(gameInfo.Player.Position.Y - tile.Y) == 1)
+                return true;
+
+         return false;
+        }
+
+        Tile FindClosest(TileContent content, Tile[,] tiles)
+        {
+            Tile closestTile = tiles[0, 0];
+            int minDistance = int.MaxValue;
+
+            for (int x = 0; x < tiles.GetLength(0); x++)
+            {
+                for (int y = 0; y < tiles.GetLength(1); y++)
+                {      
+                    Tile currentTile = tiles[x, y];
+                    if ((byte)content != currentTile.C)
+                        continue;
+
+                    int distance = Math.Abs(currentTile.X - gameInfo.Player.Position.X) + Math.Abs(currentTile.Y - gameInfo.Player.Position.Y);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestTile = currentTile;
+                    }
+                }
+            }
+            return closestTile;
+        }
+
+        Point GoToDestination(Tile destination)
+        {
+            return (GoToDestination(new Point(destination.X, destination.Y)));
+        }
+        Point GoToDestination(Point destination)
+        {
+            int dx = gameInfo.Player.Position.X - destination.X;
+            int dy = gameInfo.Player.Position.Y - destination.Y;
+            Point nextPoint = gameInfo.Player.Position;
+
+            if (Math.Abs(dx) <= Math.Abs(dy))
+            {
+                 if (dy < 0)
+                 {
+                     nextPoint = new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1);
+                 }
+                else if (dy > 0)
+                {
+                     nextPoint = new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1);
+                }
+            }
+            else
+            {
+                if (dx < 0)
+                {
+                    nextPoint = new Point(gameInfo.Player.Position.X + 1, gameInfo.Player.Position.Y);
+                }
+                else if (dx > 0)
+                {
+                    nextPoint = new Point(gameInfo.Player.Position.X - 1, gameInfo.Player.Position.Y);
+                }
+            }
+            return nextPoint;
+        }
+
     }
 }
